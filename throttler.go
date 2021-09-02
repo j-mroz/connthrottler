7a953c080a8db6ac44d55dde5b47b9d2a047368b
j-mroz <7107075+j-mroz@users.noreply.github.com>
2021-09-02 11:59:04 +0200

    initial implementation of write limiter

--- throttler.go
+++ throttler.go
@@ -1,7 +1,69 @@
 package connthrottler
 
-import "net"
+import (
+	"fmt"
+	"io"
+	"time"
 
-type Conn struct {
-	netConn net.Conn
+	"golang.org/x/time/rate"
+)
+
+var ErrNoBurstAvilable = fmt.Errorf("Burst value too low to proceed")
+
+type BytesUnit uint64
+
+const (
+	B   BytesUnit = 1
+	KiB BytesUnit = 1 << 10
+	MiB BytesUnit = 1 << 20
+	GiB BytesUnit = 1 << 30
+	TiB BytesUnit = 1 << 40
+	KB  BytesUnit = 10 << 3
+	MB  BytesUnit = 10 << 4
+	GB  BytesUnit = 10 << 5
+	TB  BytesUnit = 10 << 6
+)
+
+var siUnitsOrdered = []BytesUnit{B, KiB, MiB, GiB, TiB}
+var siUnitsNames = []string{"B", "KiB", "MiB", "GiB", "TiB"}
+
+func (b BytesUnit) String() string {
+	var pos int
+	for i, unit := range siUnitsOrdered {
+		if b/unit == 0 {
+			break
+		}
+		pos = i
+	}
+	val := float64(b) / float64(siUnitsOrdered[pos])
+	return fmt.Sprintf("%0.2f%s", val, siUnitsNames[pos])
+}
+
+type WriteLimiter struct {
+	writier io.Writer
+	limiter *rate.Limiter
+}
+
+// NewWriteLimiter decorates io.Writter with a limitter
+// with provided bytes per second value and max burst.
+func NewWriteLimiter(w io.Writer, bps, burst BytesUnit) *WriteLimiter {
+	l := rate.NewLimiter(rate.Limit(bps), int(burst))
+	return &WriteLimiter{writier: w, limiter: l}
+}
+
+// Write method limits the bytes written to at most configured amount
+// burst bytes and at the same time tries to maitian the configured bps.
+func (wl *WriteLimiter) Write(b []byte) (n int, err error) {
+	maxWriteSize := wl.limiter.Burst()
+	writeSize := len(b)
+	if maxWriteSize < writeSize {
+		writeSize = maxWriteSize
+	}
+
+	r := wl.limiter.ReserveN(time.Now(), writeSize)
+	if r.OK() {
+		time.Sleep(r.Delay())
+		return wl.writier.Write(b[:writeSize])
+	}
+	return 0, ErrNoBurstAvilable
 }
--- throttler_test.go
+++ throttler_test.go
@@ -1,6 +1,59 @@
 package connthrottler
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"testing"
+)
 
-func TestConn(t *testing.T) {
+func TestBytesUnit_String(t *testing.T) {
+	tests := []struct {
+		name string
+		b    BytesUnit
+		want string
+	}{
+		{"1B", BytesUnit(1), "1.00B"},
+		{"1KiB", 1 * KiB, "1.00KiB"},
+		{"1GiB", 1 * MiB, "1.00MiB"},
+		{"1GiB", 1 * GiB, "1.00GiB"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.b.String(); got != tt.want {
+				t.Errorf("BytesUnit.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestWriteLimiter_Write_Copy(t *testing.T) {
+	payload := make([]byte, 1*GiB)
+	tests := []struct {
+		bps           BytesUnit
+		burst         BytesUnit
+		wantWriteSize int
+		wantErr       bool
+	}{
+		{bps: 1 * KiB, burst: 1 * KiB, wantWriteSize: int(1 * GiB), wantErr: false},
+	}
+
+	for _, tt := range tests {
+		testName := fmt.Sprintf("limit-%s__burst-%s", tt.bps, tt.burst)
+		t.Run(testName, func(t *testing.T) {
+			var writeSink bytes.Buffer
+
+			wl := NewWriteLimiter(&writeSink, tt.bps, tt.burst)
+
+			written, err := io.Copy(wl, bytes.NewReader(payload))
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("io.Copy error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if written != int64(tt.wantWriteSize) {
+				t.Errorf("io.Copy write = %v, want %v", written, tt.wantWriteSize)
+			}
+		})
+	}
 }
