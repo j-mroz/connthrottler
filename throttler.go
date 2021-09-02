cfa69ccbd2e767cfefc2713c17b344c2dec70300
j-mroz <7107075+j-mroz@users.noreply.github.com>
2021-09-02 21:15:30 +0200

    add reader/writer benchmarks

--- throttler.go
+++ throttler.go
@@ -9,26 +9,31 @@ import (
 	"golang.org/x/time/rate"
 )
 
-var ErrNoBurstAvilable = fmt.Errorf("Burst value too low to proceed")
+var ErrNoBurstAvilable = fmt.Errorf("burst value too low to proceed")
 
-type BytesUnit uint64
+type ByteSize uint64
 
 const (
-	B   BytesUnit = 1
-	KiB BytesUnit = 1 << 10
-	MiB BytesUnit = 1 << 20
-	GiB BytesUnit = 1 << 30
-	TiB BytesUnit = 1 << 40
-	KB  BytesUnit = 10 << 3
-	MB  BytesUnit = 10 << 4
-	GB  BytesUnit = 10 << 5
-	TB  BytesUnit = 10 << 6
+	_            = iota
+	B   ByteSize = 1
+	KiB ByteSize = 1 << (10 * iota)
+	MiB
+	GiB
+	TiB
+	PiB
+
+	KB ByteSize = 1000
+	MB          = KB * 1000
+	GB          = MB * 1000
+	TB          = GB * 1000
+	PB          = TB * 1000
+	EB          = TB * 1000
 )
 
-var siUnitsOrdered = []BytesUnit{B, KiB, MiB, GiB, TiB}
-var siUnitsNames = []string{"B", "KiB", "MiB", "GiB", "TiB"}
+var siUnitsOrdered = []ByteSize{B, KiB, MiB, GiB, TiB, PiB}
+var siUnitsNames = []string{"B", "KiB", "MiB", "GiB", "TiB", "PiB"}
 
-func (b BytesUnit) String() string {
+func (b ByteSize) String() string {
 	var pos int
 	for i, unit := range siUnitsOrdered {
 		if b/unit == 0 {
@@ -42,17 +47,20 @@ func (b BytesUnit) String() string {
 
 // WriteLimiter decorates io.Writer with a limitter
 // with provided bytes per second value and max burst.
-// Implements io.Writer interface
+// Implements io.Writer interface.
+// Please note that resulting throughput bps depend on the value of
+// burst size and bps limit. Too small or too big burst compared to bps limit
+// causes the throughput to drop.
 type WriteLimiter struct {
 	writier io.Writer
 	// limiter *rate.Limiter
-	limit BytesUnit
-	burst BytesUnit
+	limit ByteSize
+	burst ByteSize
 }
 
 // NewWriteLimiter creates new WriteLimiter and sets
 // bytes per second and burst values in the limiter.
-func NewWriteLimiter(w io.Writer, bps, burst BytesUnit) *WriteLimiter {
+func NewWriteLimiter(w io.Writer, bps, burst ByteSize) *WriteLimiter {
 	// l := rate.NewLimiter(rate.Limit(bps), int(burst))
 	// return &WriteLimiter{writier: w, limiter: l, limit: bps, burst: burst}
 	return &WriteLimiter{writier: w, limit: bps, burst: burst}
@@ -70,8 +78,8 @@ func (wl *WriteLimiter) Write(b []byte) (n int, err error) {
 	// does not have that problem.
 	// It seems io.Copy assumes the io.Writer.Write will consume every
 	// single byte provided.
-	// TODO: creating a new reader each time is not really best performance choice.
-	rl := NewReadLimiter(bytes.NewReader(b), wl.limit, wl.burst)
+	bytesReader := bytes.NewReader(b)
+	rl := NewReadLimiter(bytesReader, wl.limit, wl.burst)
 	n2, err := io.Copy(wl.writier, rl)
 	return int(n2), err
 }
@@ -86,13 +94,13 @@ type ReadLimiter struct {
 
 // NewWriteLimiter creates new WriteLimiter and sets
 // bytes per second and burst values in the limiter.
-func NewReadLimiter(r io.Reader, bps, burst BytesUnit) *ReadLimiter {
+func NewReadLimiter(r io.Reader, bps, burst ByteSize) *ReadLimiter {
 	l := rate.NewLimiter(rate.Limit(bps), int(burst))
 	return &ReadLimiter{reader: r, limiter: l}
 }
 
 // Write method limits the bytes written to at most configured amount
-// burst bytes and at the same time tries to maitian the configured bps.
+// burst bytes and at the same time tries to maintain the configured bps.
 func (rl *ReadLimiter) Read(b []byte) (n int, err error) {
 	maxReadSize := rl.limiter.Burst()
 	readSize := len(b)
--- throttler_test.go
+++ throttler_test.go
@@ -5,15 +5,16 @@ import (
 	"fmt"
 	"io"
 	"testing"
+	"time"
 )
 
 func TestBytesUnit_String(t *testing.T) {
 	tests := []struct {
 		name string
-		b    BytesUnit
+		b    ByteSize
 		want string
 	}{
-		{"1B", BytesUnit(1), "1.00B"},
+		{"1B", 1 * B, "1.00B"},
 		{"1KiB", 1 * KiB, "1.00KiB"},
 		{"1GiB", 1 * MiB, "1.00MiB"},
 		{"1GiB", 1 * GiB, "1.00GiB"},
@@ -27,66 +28,180 @@ func TestBytesUnit_String(t *testing.T) {
 	}
 }
 
-func TestWriteLimiter_Write_Copy(t *testing.T) {
-	payload := make([]byte, 10*KiB)
+type readWriteTestArgs struct {
+	bps         ByteSize
+	burst       ByteSize
+	payloadSize ByteSize
+	wantErr     bool
+	bpsAssert   bool
+	r           io.Reader
+	w           io.Writer
+}
 
-	tests := []struct {
-		bps           BytesUnit
-		burst         BytesUnit
-		wantWriteSize int
-		wantErr       bool
-	}{
-		{bps: 1 * KiB, burst: 1 * KiB, wantWriteSize: len(payload), wantErr: false},
+type readWriteTestResult struct {
+	throughput float64
+}
+
+func TestWriteLimiter_Write_InIoCopy(t *testing.T) {
+	tests := []readWriteTestArgs{
+		{bps: 1 * KiB, burst: 512 * B, payloadSize: 2 * KiB, wantErr: false},
+		{bps: 4 * KiB, burst: 512 * B, payloadSize: 2 * KiB, wantErr: false},
+		{bps: 1 * KiB, burst: 512 * B, payloadSize: 10 * KiB, wantErr: false},
+		{bps: 4 * KiB, burst: 512 * B, payloadSize: 10 * KiB, wantErr: false},
+		{bps: 1 * MiB, burst: 512 * B, payloadSize: 10 * MiB, wantErr: false},
+		{bps: 1 * MiB, burst: 2 * KiB, payloadSize: 10 * MiB, wantErr: false},
 	}
 
+	var writeSink bytes.Buffer
+
 	for _, tt := range tests {
-		testName := fmt.Sprintf("limit-%s__burst-%s", tt.bps, tt.burst)
+		testName := fmt.Sprintf("payload-%s__limit-%s__burst-%s", tt.payloadSize, tt.bps, tt.burst)
+		tt.bpsAssert = true
+
+		payload := make([]byte, tt.payloadSize)
+		writeSink.Grow(int(tt.payloadSize))
+		writeSink.Reset()
+
 		t.Run(testName, func(t *testing.T) {
-			var writeSink bytes.Buffer
+			tt.w = NewWriteLimiter(&writeSink, tt.bps, tt.burst)
+			tt.r = bytes.NewReader(payload)
+			readWriteTest(t, &tt)
 
-			wl := NewWriteLimiter(&writeSink, tt.bps, tt.burst)
-
-			written, err := io.Copy(wl, bytes.NewReader(payload))
-
-			if (err != nil) != tt.wantErr {
-				t.Errorf("io.Copy error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if written != int64(tt.wantWriteSize) {
-				t.Errorf("io.Copy write = %v, want %v", written, tt.wantWriteSize)
-			}
 		})
 	}
 }
 
 func TestWriteLimiter_Read_InIoCopy(t *testing.T) {
-	payload := make([]byte, 10*KiB)
-
-	tests := []struct {
-		bps           BytesUnit
-		burst         BytesUnit
-		wantWriteSize int
-		wantErr       bool
-	}{
-		{bps: 1 * KiB, burst: 1 * KiB, wantWriteSize: len(payload), wantErr: false},
+	tests := []readWriteTestArgs{
+		{bps: 1 * KiB, burst: 512 * B, payloadSize: 2 * KiB, wantErr: false},
+		{bps: 4 * KiB, burst: 512 * B, payloadSize: 2 * KiB, wantErr: false},
+		{bps: 1 * KiB, burst: 512 * B, payloadSize: 10 * KiB, wantErr: false},
+		{bps: 4 * KiB, burst: 512 * B, payloadSize: 10 * KiB, wantErr: false},
 	}
-
 	for _, tt := range tests {
 		testName := fmt.Sprintf("limit-%s__burst-%s", tt.bps, tt.burst)
+		tt.bpsAssert = true
+
+		payload := make([]byte, tt.payloadSize)
+		var writeSink bytes.Buffer
+		writeSink.Grow(int(tt.payloadSize))
+
 		t.Run(testName, func(t *testing.T) {
 			readSource := bytes.NewReader(payload)
-			readLimiter := NewReadLimiter(readSource, tt.bps, tt.burst)
+			writeSink.Reset()
 
-			var writeSink bytes.Buffer
-			written, err := io.Copy(&writeSink, readLimiter)
-
-			if (err != nil) != tt.wantErr {
-				t.Errorf("io.Copy error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if written != int64(tt.wantWriteSize) {
-				t.Errorf("io.Copy write = %v, want %v", written, tt.wantWriteSize)
-			}
+			tt.r = NewReadLimiter(readSource, tt.bps, tt.burst)
+			tt.w = &writeSink
+			readWriteTest(t, &tt)
 		})
 	}
+}
+
+func Benchmark_Reader(b *testing.B) {
+	benchmarks := []readWriteTestArgs{
+		{bps: 1 * KiB, burst: 512 * B, payloadSize: 2 * KiB, wantErr: false},
+		{bps: 4 * KiB, burst: 512 * B, payloadSize: 2 * KiB, wantErr: false},
+		{bps: 1 * KiB, burst: 512 * B, payloadSize: 10 * KiB, wantErr: false},
+		{bps: 4 * KiB, burst: 512 * B, payloadSize: 10 * KiB, wantErr: false},
+	}
+
+	for _, bm := range benchmarks {
+		testName := fmt.Sprintf("payload-%s__limit-%s__burst-%s", bm.payloadSize, bm.bps, bm.burst)
+
+		var writeSink bytes.Buffer
+		writeSink.Grow(int(bm.payloadSize))
+		payload := make([]byte, bm.payloadSize)
+
+		b.Run(testName, func(b *testing.B) {
+			var avgTroughput float64
+
+			for runN := 0; runN < b.N; runN++ {
+				readSource := bytes.NewReader(payload)
+				writeSink.Reset()
+
+				bm.r = NewReadLimiter(readSource, bm.bps, bm.burst)
+				bm.w = &writeSink
+
+				testResult := readWriteTest(b, &bm)
+				avgTroughput += testResult.throughput
+			}
+
+			avgTroughput /= float64(b.N)
+
+			b.ReportMetric(avgTroughput, "B/s")
+		})
+	}
+}
+
+func Benchmark_Writer(b *testing.B) {
+	benchmarks := []readWriteTestArgs{
+		{bps: 1 * KiB, burst: 512 * B, payloadSize: 2 * KiB, wantErr: false},
+		{bps: 4 * KiB, burst: 512 * B, payloadSize: 2 * KiB, wantErr: false},
+
+		{bps: 1 * KiB, burst: 512 * B, payloadSize: 10 * KiB, wantErr: false},
+		{bps: 4 * KiB, burst: 512 * B, payloadSize: 10 * KiB, wantErr: false},
+		{bps: 1 * MiB, burst: 2 * KiB, payloadSize: 10 * MiB, wantErr: false},
+
+		{bps: 250 * MiB, burst: 30 * KiB, payloadSize: 1 * GiB, wantErr: false},
+		{bps: 250 * MiB, burst: 250 * KiB, payloadSize: 1 * GiB, wantErr: false},
+		{bps: 250 * MiB, burst: 1 * MiB, payloadSize: 1 * GiB, wantErr: false},
+		{bps: 250 * MiB, burst: 5 * MiB, payloadSize: 1 * GiB, wantErr: false},
+
+		{bps: 250 * MiB, burst: 1 * MiB, payloadSize: 1 * GiB, wantErr: false},
+		{bps: 250 * MiB, burst: 5 * MiB, payloadSize: 1 * GiB, wantErr: false},
+		{bps: 250 * MiB, burst: 25 * MiB, payloadSize: 1 * GiB, wantErr: false},
+	}
+
+	for _, bm := range benchmarks {
+		bm.bpsAssert = false
+		testName := fmt.Sprintf("payload-%s__limit-%s__burst-%s", bm.payloadSize, bm.bps, bm.burst)
+
+		var writeSink bytes.Buffer
+		writeSink.Grow(int(bm.payloadSize))
+		payload := make([]byte, bm.payloadSize)
+
+		b.Run(testName, func(b *testing.B) {
+			var avgTroughput float64
+
+			for runN := 0; runN < b.N; runN++ {
+				writeSink.Reset()
+				bm.r = bytes.NewReader(payload)
+				bm.w = NewWriteLimiter(&writeSink, bm.bps, bm.burst)
+
+				testResult := readWriteTest(b, &bm)
+				avgTroughput += testResult.throughput
+			}
+
+			avgTroughput /= float64(b.N)
+
+			b.ReportMetric(avgTroughput, "B/s")
+		})
+	}
+}
+
+func readWriteTest(tb testing.TB, args *readWriteTestArgs) (ret readWriteTestResult) {
+	copyStartTime := time.Now()
+	written, err := io.Copy(args.w, args.r)
+	copyTime := time.Since(copyStartTime)
+
+	if (err != nil) != args.wantErr {
+		tb.Errorf("io.Copy error = %v, wantErr %v", err, args.wantErr)
+		return
+	}
+	if written != int64(args.payloadSize) {
+		tb.Errorf("io.Copy write = %v, want %v", written, args.payloadSize)
+	}
+
+	throughput := float64(written) / copyTime.Seconds()
+	ret.throughput = throughput
+
+	if args.bpsAssert {
+		minBps := float64(args.bps) * 0.95
+		maxBps := float64(args.bps) * 1.05
+		if !(minBps <= throughput) || !(throughput <= maxBps) {
+			tb.Errorf("throughput = %f, accepted range [%f, %f]", throughput, minBps, maxBps)
+		}
+	}
+
+	return
 }
