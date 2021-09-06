51e4794718db9f85b3d7da964f030ba4c2511316
j-mroz <7107075+j-mroz@users.noreply.github.com>
2021-09-06 23:00:41 +0200

    add an examle server

---
+++ examples/lorem-ipsum-server/server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/j-mroz/iorate"
+)
+
+const loremIpsum = `Lorem ipsum dolor sit amet, 
+consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+deserunt mollit anim id est laborum.`
+
+func loremIpsumHandler(w http.ResponseWriter, req *http.Request) {
+	log.Println(req)
+
+	buffered := bufio.NewWriterSize(w, 2*1024*1024)
+	var err error
+	for err == nil {
+		_, err = io.WriteString(buffered, loremIpsum)
+	}
+	log.Println("request err = ", err)
+}
+
+func main() {
+	log.Println("Try me: curl http://localhost:9991/lorem-ipsum")
+	http.HandleFunc("/lorem-ipsum", loremIpsumHandler)
+
+	listener, err := net.Listen("tcp", "localhost:9991")
+	if err != nil {
+		panic(err)
+	}
+
+	limitedListener := iorate.NewLimitedListener(listener)
+
+	listenerLimit := 1 * iorate.MBps
+	connLimit := 250 * iorate.KBps
+	limitedListener.SetLimits(listenerLimit, connLimit)
+
+	log.Println("listener limit:", listenerLimit)
+	log.Println("conn limit:", connLimit)
+
+	http.Serve(limitedListener, nil)
+}
